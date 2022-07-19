b35a2519769c46f46965833680da2375602f70cc
Joel Nuesch <jnuesch@protonmail.com>
2022-07-19 00:23:53 -0700

    Stricter ts conf

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,26 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
+  root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
   },
   plugins: [
     '@typescript-eslint',
   ],
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
   rules: {
+    '@typescript-eslint/no-floating-promises': 0,
+    'class-methods-use-this': 0,
+    'import/extensions': 0,
+    'import/prefer-default-export': 0,
+    'no-param-reassign': 0,
+    'no-underscore-dangle': 0,
   },
+  ignorePatterns: ["dist", "**/*.js"],
 };
