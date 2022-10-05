a74cfce2b43fad2a864a305d504e98a92223fd83
Joel Nuesch <jnuesch@protonmail.com>
2022-10-05 14:03:35 -0700

    Implement rehydration handler

--- src/CachedQuery/rehydration/handler.ts
+++ src/CachedQuery/rehydration/handler.ts
@@ -1,8 +1,8 @@
 import { EJSON } from 'bson';
 import type Mondis from 'src/mondis';
+import type CachedQuery from '..';
 
 const MATCH_CACHE_KEY = /^Q:(.+?)(\[.*\])$/;
-
 function parseCacheKey(key: string) {
   const [, hash, params] = key.match(MATCH_CACHE_KEY) || [];
   if (!hash || !params) throw Error('Failed to parse cache key');
@@ -13,24 +13,38 @@ function parseCacheKey(key: string) {
 }
 
 export default class RehydrationHandler {
+  private lookupCachedQuery?: Map<string, CachedQuery>;
+
   constructor(
     readonly context: Mondis,
   ) { }
 
-  private async rehydrate(keys: string[]) {
+  async rehydrate(keys: string[]) {
     if (!keys.length) return;
-    const { queries } = this.context;
 
-    Object.values(queries).forEach((query) => {
+    const promises = keys.map(async (key) => {
+      const { hash, params } = parseCacheKey(key);
+      const cachedQuery = this.findCachedQuery(hash);
+      if (!cachedQuery || !cachedQuery.config.rehydrate) return;
+
+      await cachedQuery.exec({
+        params,
+        limit: cachedQuery.config.cacheCount,
+        skipCache: true,
+      });
     });
-    // const { lookupCachedQuery } = this.context;
-    // const promises = keys.map(async (key) => {
-    //   const { hash, params } = parseCacheKey(key);
-    //   const query = lookupCachedQuery.get(hash);
-    //   if (!query || !query.config.rehydrate) return;
-    //   await query.exec({ params, limit: query.config.cacheCount, skipCache: true });
-    // });
 
-    // await Promise.all(promises);
+    await Promise.all(promises);
+  }
+
+  private findCachedQuery(hash: string) {
+    if (!this.lookupCachedQuery) {
+      const map = new Map<string, CachedQuery>();
+      Object.values(this.context.queries).forEach((query) => {
+        map.set(query.hash, query);
+      });
+      this.lookupCachedQuery = map;
+    }
+    return this.lookupCachedQuery.get(hash);
   }
 }
