6c83d063b9dedc42648fceb9274f829772cb2d66
Joel Nuesch <jnuesch@protonmail.com>
2022-10-09 18:41:21 -0700

    Move projection tests/comments

--- tests/invalidation.ts
+++ tests/invalidation.ts
@@ -14,3 +14,20 @@ Object.entries(MongooseUpdates).forEach(([name, execUpdate]) => {
 });
 
 // TODO: test to ensure keys are actually invalidated.
+
+// TODO: utils unit tests
+// testCase({ inclusive: false, paths: [] }, [], false);
+// testCase({ inclusive: false, paths: [] }, ['lol'], true);
+// testCase({ inclusive: true, paths: ['hello'] }, ['hello.world'], true);
+// testCase({ inclusive: true, paths: ['hello.world'] }, ['hello.world'], true);
+// testCase({ inclusive: true, paths: ['hello.world'] }, ['hello.lol'], false);
+// testCase({ inclusive: true, paths: ['hello.world'] }, ['hello'], true);
+// testCase({ inclusive: true, paths: ['hello', 'lol'] }, ['lol'], true);
+// testCase({ inclusive: true, paths: ['hello', 'lol'] }, ['nah'], false);
+// testCase({ inclusive: false, paths: ['hello'] }, ['hello.world'], false);
+// testCase({ inclusive: false, paths: ['hello.world'] }, ['hello.world'], false);
+// testCase({ inclusive: false, paths: ['hello.world'] }, ['hello.lol'], true);
+// testCase({ inclusive: false, paths: ['hello', 'lol'] }, ['hello', 'lol'], false);
+// testCase({ inclusive: false, paths: ['hello', 'lol'] }, ['hello', 'lol', 'cool'], true);
+// testCase({ inclusive: false, paths: ['hello.world'] }, ['hello'], true);
+// testCase({ inclusive: false, paths: ['hello.world'] }, ['hello.world.ok'], false);
